f5824a4b97bf09631c5543ed30d63688187fe7f5
Emzzzy <up2529611@myport.ac.uk>
2026-01-16 10:14:10 +0000

    Added files

---
+++ sfcc notes/Week 3 FLOSS SFCC/testscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script will create a new folder and populate it 
+
+mkdir library_project
+cd library_project
+touch library.txt
+date >> library.txt
+echo 'All finished!'
---
+++ sfcc notes/week4/interactive.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+read -p "What are you having for tea? : " FOOD
+
+echo "hey $USER, you are having $FOOD for tea"
